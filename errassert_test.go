f61f13cf2d945e43c8c2b5f4cc790ec248e68e86
Luboš Pokorný <zoido@users.noreply.github.com>
2024-03-12 18:29:56 +0100

    feat: 🚧 Define the interface of the error assertion (#8)

---
+++ errassert_test.go
@@ -0,0 +1,83 @@
+package errassert_test
+
+import (
+	"testing"
+
+	"github.com/zoido/errassert"
+)
+
+func NewMockT() *MockT {
+	return &MockT{}
+}
+
+type MockT struct {
+	failCalled    bool
+	failNowCalled bool
+	logfCalled    bool
+	logfMsg       string
+}
+
+var _ errassert.TestingT = (*MockT)(nil)
+
+func (m *MockT) Helper()  {}
+func (m *MockT) FailNow() { m.failNowCalled = true }
+func (m *MockT) Fail()    { m.failCalled = true }
+
+func (m *MockT) Log(msg string) {
+	m.logfCalled = true
+	m.logfMsg = msg
+}
+
+func (m MockT) AssertFailed(t *testing.T) {
+	t.Helper()
+	if !m.failCalled {
+		t.Error("expected Fail to be called but it was not")
+	}
+}
+
+func (m MockT) AssertFailedNow(t *testing.T) {
+	t.Helper()
+	if !m.failNowCalled {
+		t.Error("expected FailNow to be called but it was not")
+	}
+}
+
+func (m MockT) AssertNotFailed(t *testing.T) {
+	t.Helper()
+	if m.failCalled {
+		t.Error("expected FailNow to not be called but it was")
+	}
+}
+
+func (m MockT) AssertNotFailedNow(t *testing.T) {
+	t.Helper()
+	if m.failNowCalled {
+		t.Error("expected FailNow to not be called but it was")
+	}
+}
+
+func (m MockT) AssertLogfCalled(t *testing.T) {
+	t.Helper()
+	if !m.logfCalled {
+		t.Error("expected Logf to be called but it was not")
+	}
+}
+
+func (m MockT) AssertLogfNotCalled(t *testing.T) {
+	t.Helper()
+	if m.logfCalled {
+		t.Error("expected Logf to not be called but it was")
+	}
+}
+
+func (m MockT) AssertLogfCalledWith(t *testing.T, expected string) {
+	t.Helper()
+	m.AssertLogfCalled(t)
+	if m.logfMsg != expected {
+		t.Errorf(
+			"expected Logf to be called with %q but it was called with %q",
+			expected,
+			m.logfMsg,
+		)
+	}
+}
---
+++ errorassertion.go
@@ -0,0 +1,31 @@
+package errassert
+
+// ErrorAssertion represents single single instance of the error assertion.
+// If the error is not as expected, call returns an error.
+type ErrorAssertion func(error) error
+
+// TestingT defines the subset of testing.TB methods used by errassert.
+type TestingT interface {
+	Log(string)
+	Fail()
+	FailNow()
+	Helper()
+}
+
+// Assert checks if the error is as expected and calls t.Errorf if not.
+func (assertion ErrorAssertion) Assert(t TestingT, err error, _ ...interface{}) {
+	t.Helper()
+	if tErr := assertion(err); tErr != nil {
+		t.Log(tErr.Error())
+		t.Fail()
+	}
+}
+
+// Require checks if the error is as expected and calls t.FailNow if not.
+func (assertion ErrorAssertion) Require(t TestingT, err error, _ ...interface{}) {
+	t.Helper()
+	if tErr := assertion(err); tErr != nil {
+		t.Log(tErr.Error())
+		t.FailNow()
+	}
+}
