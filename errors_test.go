155d5651ffbb69b669e37f249184dfbf6f65a585
Luboš Pokorný <zoido@users.noreply.github.com>
2024-03-13 07:30:05 +0100

    feat: 🚧 go1.13 ErrorIs/ErrorAs assertions (#12)

--- errors_test.go
+++ errors_test.go
@@ -71,6 +71,86 @@ func TestNoError(t *testing.T) {
 	}
 }
 
+func TestErrorIs(t *testing.T) {
+	type testCase struct {
+		in   error
+		want error
+	}
+
+	target := errors.New("expected error")
+
+	run := func(t *testing.T, tc testCase) {
+		// When
+		got := errassert.ErrorIs(target)(tc.in)
+
+		// Then
+		if !errEqual(got, tc.want) {
+			t.Errorf("ErrorIs(%v) = %v; want %v", tc.in, got, tc.want)
+		}
+	}
+
+	testCases := map[string]testCase{
+		"nil fails": {
+			in:   nil,
+			want: errors.New("expected error to be 'expected error' but got '<nil>'"),
+		},
+		"not matching fails": {
+			in:   errors.New("another error"),
+			want: errors.New("expected error to be 'expected error' but got 'another error'"),
+		},
+		"matching passes": {
+			in:   target,
+			want: nil,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) { run(t, tc) })
+	}
+}
+
+func TestErrorAs(t *testing.T) {
+	type testCase struct {
+		in   error
+		want error
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		// When
+		got := errassert.ErrorAs(new(testError))(tc.in)
+
+		// Then
+		if !errEqual(got, tc.want) {
+			t.Errorf("ErrorAs(%v) = %v; want %v", tc.in, got, tc.want)
+		}
+	}
+
+	testCases := map[string]testCase{
+		"nil fails": {
+			in:   nil,
+			want: errors.New("expected error to be '*errassert_test.testError' but got '<nil>'"),
+		},
+		"not matching fails": {
+			in: errors.New("another error"),
+			want: errors.New(
+				"expected error to be '*errassert_test.testError' but got '*errors.errorString'",
+			),
+		},
+		"matching passes": {
+			in:   testError{},
+			want: nil,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) { run(t, tc) })
+	}
+}
+
+type testError struct{}
+
+func (testError) Error() string { return "test error" }
+
 func errEqual(a, b error) bool {
 	if a == nil && b == nil {
 		return true
