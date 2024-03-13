767856893c1b3ba5454d454f2d2e113c016273bb
Luboš Pokorný <zoido@users.noreply.github.com>
2024-03-13 07:47:22 +0100

    feat: 🚧 basic string based assertions (#15)

--- errors.go
+++ errors.go
@@ -3,6 +3,7 @@ package errassert
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 // SomeError returns an assertion that checks if the error is not nil.
@@ -40,6 +41,45 @@ func ErrorAs(target interface{}) ErrorAssertion {
 	return func(err error) error {
 		if !errors.As(err, target) {
 			return fmt.Errorf("expected error to be '%T' but got '%T'", target, err)
+		}
+		return nil
+	}
+}
+
+// ErrorContains returns an assertion that checks if the error contains the given substring.
+func ErrorContains(substring string) ErrorAssertion {
+	return func(err error) error {
+		if err == nil {
+			return fmt.Errorf("expected an error containing '%s' but got nil", substring)
+		}
+		if !strings.Contains(err.Error(), substring) {
+			return fmt.Errorf("expected an error containing '%s' but got '%v'", substring, err)
+		}
+		return nil
+	}
+}
+
+// ErrorStartsWith returns an assertion that checks if the error starts with the given prefix.
+func ErrorStartsWith(prefix string) ErrorAssertion {
+	return func(err error) error {
+		if err == nil {
+			return fmt.Errorf("expected an error starting with '%s' but got nil", prefix)
+		}
+		if !strings.HasPrefix(err.Error(), prefix) {
+			return fmt.Errorf("expected an error starting with '%s' but got '%v'", prefix, err)
+		}
+		return nil
+	}
+}
+
+// ErrorEndsWith returns an assertion that checks if the error ends with the given suffix.
+func ErrorEndsWith(suffix string) ErrorAssertion {
+	return func(err error) error {
+		if err == nil {
+			return fmt.Errorf("expected an error ending with '%s' but got nil", suffix)
+		}
+		if !strings.HasSuffix(err.Error(), suffix) {
+			return fmt.Errorf("expected an error ending with '%s' but got '%v'", suffix, err)
 		}
 		return nil
 	}
